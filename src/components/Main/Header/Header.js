d15bdbf7907ff600517a97ed04849519975d90f6
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-24 17:33:20 +0100

    housekeeping

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
         <Col xs={12} sm={6}>
           <MainTitle>To-Do List</MainTitle>
         </Col>
-        <Col xs={12} sm={6}>
+        <Col xs={12} sm={6} className="Header-align-right">
           <SearchForm />
         </Col>
       </Row>
